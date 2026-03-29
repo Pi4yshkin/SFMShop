e2264b77fa01a63092cda26318f07ffe13c50cd8
Alex Pi4yshkin <spk60150@gmail.com>
2026-03-29 10:16:52 +0300

    отредактирована проверка подключения к БД

--- main.py
+++ main.py
@@ -4,32 +4,36 @@ from src.database.connection import connect_to_db, get_user_by_id, create_user, 
 
 try:
     conn = connect_to_db("localhost", "sfmshop", "postgres", "DiggerLLP199222")
+
+
+    if conn:
+        try:
+            user = create_user(conn, "Maria", "maria@test.com")
+            print(user)
+        except Exception as e:
+            print(f"Ошибка добавления пользователя: {e}")
+
+
+        user = get_user_by_id(conn, 63)
+        print(f"Пользователь найден: {user}")
+
+
+        try:
+            order = create_order(conn, 63, 76500)
+            print(order)
+        except Exception as e:
+            print(f"Ошибка: {e}")
+
+
+        try:
+            user_orders = get_user_orders(conn, 63)
+            print("Заказы пользователя: ", end="")
+            for order in user_orders:
+                print(*order)
+        except Exception as e:
+            print(f"Ошибка: {e}")
+
+    conn.close()
 except OperationalError as e:
     print(f"Ошибка: {e}")
 
-
-try:
-    user = create_user(conn, "Alex", "alex@test.com")
-    print(user)
-except Exception as e:
-    print(f"Ошибка добавления пользователя: {e}")
-
-
-user = get_user_by_id(conn, 63)
-print(f"Пользователь найден: {user}")
-
-
-try:
-    order = create_order(conn, 63, 76500)
-    print(order)
-except Exception as e:
-    print(f"Ошибка: {e}")
-
-try:
-    user_orders = get_user_orders(conn, 63)
-    print("Заказы пользователя: ", end="")
-    for order in user_orders:
-        print(*order)
-except Exception as e:
-    print(f"Ошибка: {e}")
-    conn.close()
